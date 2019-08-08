bfefc429ceb8961daac5c59322169e9299ef5cc2
Amy Westcott <amy.westcott@yahoo.com>
2019-08-08 19:45:11 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,5 +20,9 @@ class App < Sinatra::Base
      @user_name = params[:name]
     "Goodbye, #{@user_name}."
   end 
+  
+  get "multiply/:num1/:num2" do 
+    @num1 = params[:num1].to_i
+  end 
 
 end
