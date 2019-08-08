4c957b8411da9060c05872c13f982a164821a936
Amy Westcott <amy.westcott@yahoo.com>
2019-08-08 19:47:02 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -21,7 +21,7 @@ class App < Sinatra::Base
     "Goodbye, #{@user_name}."
   end 
   
-  get "multiply/:num1/:num2" do 
+  get "/multiply/:num1/:num2 do" 
     @answer = params[:num1].to_i * params[:num2].to_i
     "#{@answer}"
   end 
